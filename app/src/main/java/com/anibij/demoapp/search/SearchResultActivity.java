7f502869fe21d2268aadcc87935925e7d8f1b6f1
bsoren <bijay.soren@gmail.com>
2016-07-03 12:07:30 +0530

    Added searchfunctionality package

---
+++ app/src/main/java/com/anibij/demoapp/search/SearchResultActivity.java
@@ -0,0 +1,15 @@
+package com.anibij.demoapp.search;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.anibij.demoapp.R;
+
+public class SearchResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result);
+    }
+}
